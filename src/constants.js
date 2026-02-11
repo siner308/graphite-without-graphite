dac0a92b1ecffe340e1ca009dfb03895769eca91
siner308 <jeonghyun.an@devsisters.com>
2026-02-11 13:46:57 +0900

    Add constants module

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+// Stack Queue constants
+
+const RETRY_INTERVAL_MS = 30000;
+const MAX_RETRIES = 20;
+const STACK_META_PATTERN = /<!-- stack-rebase:([\s\S]*?) -->/;
+
+module.exports = { RETRY_INTERVAL_MS, MAX_RETRIES, STACK_META_PATTERN };
